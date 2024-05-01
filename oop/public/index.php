ebedba7b8d39e6c1618e09be8804981dea150909
unknown <recona97@gmail.com>
2024-05-01 15:55:21 +0900

    Learn DateTime object

--- oop/public/index.php
+++ oop/public/index.php
@@ -81,3 +81,39 @@ $invoice = new Invoice(25, "Hello, PHP!");
 // Do NOT use Serializable interface. It will be deprcated.
 // Instead, use `sleep` & `wakeup` magic method.
 echo serialize($invoice) . "<br/>";
+
+echo "Error Handling in OOP" . "<br/>";
+
+echo "DateTime in OOP" . "<br/>";
+
+$dateTime = new DateTime("tomorrow 3:35pm");
+inspect($dateTime);
+inspect($dateTime->getTimezone()->getName() . " - " . $dateTime->format("m/d/Y g:i A"));
+
+// day/month/year - europe
+// month/day/year - U.S.
+// What to be passed datetime data in different formats.
+$date1 = "15/05/2023 03:32";
+$dateTime = DateTime::createFromFormat("d/m/Y g:iA", $date1);
+inspect($dateTime);
+
+$date1 = "15/05/2023";
+$dateTime = DateTime::createFromFormat("d/m/Y", $date1)->setTime(0, 0); // If you don't specify time, it will use current time. But new DateTime won't.
+inspect($dateTime);
+
+$date1 = "05/25/2023 3:12 AM";
+$date2 = "03/30/2024 8:40 PM";
+$dateTime1 = new DateTime($date1);
+$dateTime2 = new DateTime($date2);
+inspect($dateTime1->diff($dateTime2));
+inspect($dateTime1->diff($dateTime2)->format("%R %Y years, %m months, %d days, %H, %i, %s")); // Refer to PHP official document
+
+// DateTimeImmutable
+
+// $period = new DatePeriod(new DateTime("05/01/2023"), new DateInterval("P1D"), new DateTime("05/31/2023"));
+$period = new DatePeriod(new DateTime("05/01/2023"), new DateInterval("P1D"), 4, DatePeriod::EXCLUDE_START_DATE);
+
+// $period is not array
+foreach ($period as $date) {
+  inspect($date->format("m/d/Y"));
+}
